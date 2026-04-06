ecf12ed0f9dfde22ec0cd927147a0291f3d427f8
dzaky01M <mdzakynm@student.ub.ac.id>
2026-04-06 16:30:44 +0700

    Add files via upload

---
+++ MedicalRecord.java
@@ -0,0 +1,5 @@
+// Kontrak dasar semua data medis 
+public interface MedicalRecord { 
+    String getPatientId(); 
+}
+
---
+++ Versioned.java
@@ -0,0 +1,4 @@
+// Kontrak untuk versi data 
+public interface Versioned { 
+    int getVersion(); 
+} 
