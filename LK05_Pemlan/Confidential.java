71186b845a6e3b699fb6f05f3fa0d0aa8a096f22
dzaky01M <mdzakynm@student.ub.ac.id>
2026-04-07 19:53:44 +0700

    Add files via upload

---
+++ LK05_Pemlan/Confidential.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Hanif Maulana
+ */
+public interface Confidential {
+    int getSecurityLevel();
+    /*
+    Contoh : 
+    1. Public 
+    2. Restricted
+    3. Secret
+    */
+    
+    void maskSensititveData(); // sesuaikan
+}
